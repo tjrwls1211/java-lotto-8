73173e793cc69302fa9362a080a2182aa5e84213
tjrwls1211 <seokjin6635@gmail.com>
2025-10-31 23:45:01 +0900

    test(domain): 로또 번호가 1 ~ 45 사이의 숫자가 아닌 경우 예외 테스트 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -21,4 +21,12 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 로또 번호는 중복될 수 없습니다.");
     }
+
+    @DisplayName("로또 번호가 1과 45 사이의 숫자가 아니라면 예외가 발생한다.")
+    @Test
+    void 로또_번호가_1과_45_사이의_숫자가_아니라면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1,2,3,4,5,46)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 로또 번호는 1과 45 사이의 숫자여야 합니다.");
+    }
 }
