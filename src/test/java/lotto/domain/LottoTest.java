19dce12fba1a561ea80914b22b18952548b36151
tjrwls1211 <seokjin6635@gmail.com>
2025-10-31 23:25:38 +0900

    test(domain): 로또 번호 중복 시 예외 에러 메시지 검증 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,4 +1,4 @@
-package lotto;
+package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,8 +18,7 @@ class LottoTest {
     @Test
     void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 로또 번호는 중복될 수 없습니다.");
     }
-
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
 }
