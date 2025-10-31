8c0f10c733dd2e96b3fc94e09c12eca93b00cbaa
tjrwls1211 <seokjin6635@gmail.com>
2025-10-31 23:45:46 +0900

    feat(domain): 로또 번호 범위 유효성 검사 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -20,10 +20,18 @@ public class Lotto {
         if (hasDuplicate(numbers)) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
         }
+
+        if (!hasValidRange(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1과 45 사이의 숫자여야 합니다.");
+        }
     }
 
     private boolean hasDuplicate(List<Integer> numbers) {
         Set<Integer> checkNumbers = new HashSet<>(numbers);
         return checkNumbers.size() != numbers.size();
+    }
+
+    private boolean hasValidRange(List<Integer> numbers) {
+        return numbers.stream().allMatch(number -> number >= 1 && number <= 45);
     }
 }
